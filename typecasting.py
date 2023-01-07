4689580260cfd589062240bd4d5fb03174e59294
Aman Lath <85346421+Amanlath1@users.noreply.github.com>
2023-01-07 14:28:45 +0530

    learned typecasting in python

---
+++ typecasting.py
@@ -0,0 +1,6 @@
+num1 = input("Enter number 1 : ")
+num2 = input("Enter number 2 : ")
+ans1 = num1 + num2
+ans11 = (int(num1) + int(num2))
+print("The addition of both the numbers without typecasting :  ", ans1)
+print("The addition of both the numbers with typecasting :", ans11)
