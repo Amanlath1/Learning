f184afcd53cbd7275f1aba95c67125e812f7557a
Aman Lath <85346421+Amanlath1@users.noreply.github.com>
2023-01-07 13:35:44 +0530

    created a min calculator in python

---
+++ mini_calculator.py
@@ -0,0 +1,14 @@
+num1=10
+num2=20
+ans1 = num1 + num2
+print("The addition of both the numbers is ", ans1)
+ans2 = num2 - num1
+print("The Substraction of both the numbers is ", ans2)
+ans3 = num1 * num2
+print("The multiplication of both the numbers is ", ans3)
+ans4 = num2 / num1
+print("The division of both the numbers is ", ans4)
+ans5 = num2 % num1
+print("The remainder of both the numbers is ", ans5)
+ans6 = num2 ** num1
+print("The power multiplication of both the numbers is ", ans6)
